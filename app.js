dfba63769d53681cd0de4c6cfdf34b74df7c58f2
Michaeltronik <michaelrts@hotmail.com>
2024-08-06 11:48:56 -0500

    correccion texto no válido

--- app.js
+++ app.js
@@ -1,139 +1,139 @@
-
-function validarTexto(texto){
-    const  regex = /^[a-z\s]*$/; //expresion regular solo minusculas y espacios
-    return regex.test(texto);
+let valido = true;
+function validarTexto(texto) {
+  const regex = /^[a-z\s]*$/; //expresion regular solo minusculas y espacios
+  return regex.test(texto);
 }
-
 
 function encriptarTexto() {
   let texto = document.getElementById("inputText").value.trim(); //elimina espacios al inicio y final y obtiene el texto ingresado
-  
-  if (texto === '') {
+
+  if (texto === "") {
     alert("Lo siento, no se ha detectado un texto");
     verificarEntradaDeTexto();
-    return; 
-}
-  if (validarTexto(texto)){
-    let encriptacion = "";
-  for (let i = 0; i < texto.length; i++) {
-    let letra = texto[i];
-    switch (letra) {
-      case "a":
-        letra = "ai";
-        break;
-      case "e":
-        letra = "enter";
-        break;
-      case "i":
-        letra = "imes";
-        break;
-      case "o":
-        letra = "ober";
-        break;
-      case "u":
-        letra = "ufat";
-        break;
-    }
-    encriptacion += letra;
-   
+    return;
   }
-  actualizarIntefaz();
-  //document.getElementById('inputText').value='';
+  if (validarTexto(texto)) {
+    let encriptacion = "";
+    for (let i = 0; i < texto.length; i++) {
+      let letra = texto[i];
+      switch (letra) {
+        case "a":
+          letra = "ai";
+          break;
+        case "e":
+          letra = "enter";
+          break;
+        case "i":
+          letra = "imes";
+          break;
+        case "o":
+          letra = "ober";
+          break;
+        case "u":
+          letra = "ufat";
+          break;
+      }
+      encriptacion += letra;
+    }
+    actualizarIntefaz();
+    //document.getElementById('inputText').value='';
 
-  return encriptacion;
-}
-else{
-    alert("Por favor ingrese un texto válido, solo se aceptan minusculas sin acentos");
-    document.getElementById('inputText').value='';
-}
+    valido = true;
+    return encriptacion;
+  } else {
+    alert(
+      "Por favor ingrese un texto válido, solo se aceptan minusculas sin acentos"
+    );
+    valido = false;
+    verificarEntradaDeTexto();
+  }
 }
 function encriptarYMostrarResultado() {
-    let outputText = document.getElementById('outputText');
-    let textoEncriptado=encriptarTexto();
-   // console.log(textoEncriptado);
-    outputText.value= textoEncriptado;
-    
+  let outputText = document.getElementById("outputText");
+  let textoEncriptado = encriptarTexto();
+  // console.log(textoEncriptado);
+  outputText.value = textoEncriptado;
 }
 function desencriptarTexto() {
-    let texto = document.getElementById('inputText').value.trim();
-    if (texto === '') {
-        alert("Lo siento, no se ha detectado un texto");
-        verificarEntradaDeTexto();
-        return; 
-    }
-    if(validarTexto(texto)){
+  let texto = document.getElementById("inputText").value.trim();
+  if (texto === "") {
+    alert("Lo siento, no se ha detectado un texto");
+    verificarEntradaDeTexto();
+    return;
+  }
+  if (validarTexto(texto)) {
     let textoDesencriptado;
-    textoDesencriptado = texto.replaceAll("enter","e");
-    textoDesencriptado = textoDesencriptado.replaceAll("imes","i");
-    textoDesencriptado = textoDesencriptado.replaceAll("ai","a");
-    textoDesencriptado = textoDesencriptado.replaceAll("ober","o");
-    textoDesencriptado = textoDesencriptado.replaceAll("ufat","u");
-   // document.getElementById('inputText').value='';
-  
+    textoDesencriptado = texto.replaceAll("enter", "e");
+    textoDesencriptado = textoDesencriptado.replaceAll("imes", "i");
+    textoDesencriptado = textoDesencriptado.replaceAll("ai", "a");
+    textoDesencriptado = textoDesencriptado.replaceAll("ober", "o");
+    textoDesencriptado = textoDesencriptado.replaceAll("ufat", "u");
+    // document.getElementById('inputText').value='';
+
     actualizarIntefaz();
 
     return textoDesencriptado;
-}
-    else{
-        alert("Por favor ingrese un texto valido,solo se aceptan minusculas sin acentos");
-        document.getElementById('inputText').value='';
-
-        return ;
-    }
+  } else {
+    alert(
+      "Por favor ingrese un texto valido,solo se aceptan minusculas sin acentos"
+    );
+    valido = false;
+    verificarEntradaDeTexto();
+    return;
+  }
 }
 function desencriptarYMostrarResultado() {
-    let outputText = document.getElementById('outputText');
-    let textoDesencriptado=desencriptarTexto();
-   // console.log("Resultado desencriptado:", textoDesencriptado);
-    outputText.value=textoDesencriptado;
+  let outputText = document.getElementById("outputText");
+  let textoDesencriptado = desencriptarTexto();
+  // console.log("Resultado desencriptado:", textoDesencriptado);
+  outputText.value = textoDesencriptado;
 }
 //validar si el textarea esta en blanco para mostrar la imagen
-function verificarEntradaDeTexto(){                                      
-    let texto = document.getElementById("inputText").value;
-    let outputText = document.getElementById('outputText');
-    let textoEstado = document.getElementById('mensajeNoEncontrado');
-    let solicitudTexto= document.getElementById('escribeTexto')
-    let img = document.getElementById('imagenSalida');
+function verificarEntradaDeTexto() {
+  let texto = document.getElementById("inputText").value;
+  let outputText = document.getElementById("outputText");
+  let textoEstado = document.getElementById("mensajeNoEncontrado");
+  let solicitudTexto = document.getElementById("escribeTexto");
+  let img = document.getElementById("imagenSalida");
 
-    let botonCopiar = document.getElementById('copiar');
-    let botonBorrar = document.getElementById('borrar');
+  let botonCopiar = document.getElementById("copiar");
+  let botonBorrar = document.getElementById("borrar");
 
-
-    if (texto.trim() ===''){
-        img.style.display = 'block';
-        textoEstado.style.display='block';
-        solicitudTexto.style.display='block';
-        outputText.style.display='none';
-        botonCopiar.style.display='none';
-        botonBorrar.style.display='none';
-        outputText.value = '';
-    }
+  if (texto.trim() === "" || valido == false) {
+    img.style.display = "block";
+    textoEstado.style.display = "block";
+    solicitudTexto.style.display = "block";
+    outputText.style.display = "none";
+    botonCopiar.style.display = "none";
+    botonBorrar.style.display = "none";
+    outputText.value = "";
+  }
 }
-function limpiarTexto(){
-    document.getElementById('inputText').value='';
-    document.getElementById('outputText').value='';
-    verificarEntradaDeTexto();
+function limpiarTexto() {
+  document.getElementById("inputText").value = "";
+  document.getElementById("outputText").value = "";
+  verificarEntradaDeTexto();
 }
-async function copiarTexto(){
-    try {
-        let texto = document.getElementById('outputText');
-        await navigator.clipboard.writeText(texto.value);
-      } catch (err) {
-        console.error('Error al copiar al portapapeles:', err);
-      }
+async function copiarTexto() {
+  try {
+    let texto = document.getElementById("outputText");
+    await navigator.clipboard.writeText(texto.value);
+  } catch (err) {
+    console.error("Error al copiar al portapapeles:", err);
+  }
 }
 // si se ha encriptado texto, muestra el resultado y quita la imagen y habilita los botones de copiar y limpiar
-function actualizarIntefaz(){
-    let textoEstado = document.getElementById('mensajeNoEncontrado');
-    let solicitudTexto= document.getElementById('escribeTexto')
-    let img = document.getElementById('imagenSalida');
-    let botonCopiar = document.getElementById('copiar');
-    let botonBorrar = document.getElementById('borrar');
+function actualizarIntefaz() {
+  let textoEstado = document.getElementById("mensajeNoEncontrado");
+  let solicitudTexto = document.getElementById("escribeTexto");
+  let img = document.getElementById("imagenSalida");
+  let botonCopiar = document.getElementById("copiar");
+  let botonBorrar = document.getElementById("borrar");
 
-    img.style.display = 'none';
-    textoEstado.style.display = 'none';
-    solicitudTexto.style.display = 'none';
-    outputText.style.display = 'block';
-    botonCopiar.style.display = 'block';
-    botonBorrar.style.display = 'block';
+  img.style.display = "none";
+  textoEstado.style.display = "none";
+  solicitudTexto.style.display = "none";
+  outputText.style.display = "block";
+  botonCopiar.style.display = "block";
+  botonBorrar.style.display = "block";
 }
